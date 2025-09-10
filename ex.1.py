8fab558c8ed26d11c54003434268d3cd974ed152
karth <karhikdheeravath718@gmail.com>
2025-09-10 15:27:09 +0530

    added

--- ex.1.py
+++ ex.1.py
@@ -1,7 +1,11 @@
 print("My Name Is Mounika")
 print("My Name Is Mounika Iam From Bollaram")
-<<<<<<< HEAD
-=======
 
-print("Kukatpalle")
->>>>>>> f1e6da00bcad0df0a73613abe07fbdface689eb1
+
+print("My Name Is Mounika")
+print("My Name Is Mounika Iam From Bollaram")
+
+
+print("My Name Is Mounika")
+print("My Name Is Mounika Iam From Bollaram")
+
